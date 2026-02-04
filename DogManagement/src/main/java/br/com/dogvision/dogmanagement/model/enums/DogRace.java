57d8fb427358f215f3801e072b73dda028dc9393
Pedro <rocha.pedrohenrique91@gmail.com>
2026-02-04 16:39:48 -0300

    feat: created an enum DogRace and DogStatus

---
+++ DogManagement/src/main/java/br/com/dogvision/dogmanagement/model/enums/DogRace.java
@@ -0,0 +1,9 @@
+package br.com.dogvision.dogmanagement.model.enums;
+
+
+public enum DogRace {
+    GOLDEN_RETRIEVER,
+    PASTOR_ALEMAO,
+    Border_Collie,
+    Labrador
+}
---
+++ DogManagement/src/main/java/br/com/dogvision/dogmanagement/model/enums/DogStatus.java
@@ -0,0 +1,10 @@
+package br.com.dogvision.dogmanagement.model.enums;
+
+public enum DogStatus {
+    PRE_SOCIALIZACAO,
+    SOCIALIZACAO,
+    TREINAMENTO,
+    ADAPTACAO,
+    DOACAO,
+    CEDIDO
+}
