53d5f0df1e15f4af816b9506f8e02e4bced5a5bd
Pedro <rocha.pedrohenrique91@gmail.com>
2026-03-20 09:20:09 -0300

    ADDED APPLICATION.JAVA AND TESTES FROM DOGHEALTH, DOGMANAGEMENT, EUREKASERVER

---
+++ DogHealth/src/main/java/br/com/dogvision/doghealth/DogHealthApplication.java
@@ -0,0 +1,13 @@
+package br.com.dogvision.doghealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogHealthApplication.class, args);
+    }
+
+}
---
+++ DogHealth/src/test/java/br/com/dogvision/doghealth/DogHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dogvision.doghealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogHealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
